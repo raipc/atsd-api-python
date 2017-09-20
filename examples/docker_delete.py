444b0896392c5fd75f03d4514388b4d6f1167814
Anton Rybochkin <anton.rybochkin@axibase.com>
2017-09-20 13:57:10 +0300

    replaced string format with concatenation

--- examples/docker_delete.py
+++ examples/docker_delete.py
@@ -16,7 +16,7 @@ conn = connect_url('http://atsd_hostname:8088', 'user', 'pwd')
 entity_service = EntitiesService(conn)
 docker_hosts = conn.get("v1/metrics/docker.cpu.sum.usage.total.percent/series")
 
-print("Docker hosts found: {}".format(len(docker_hosts)))
+print("Docker hosts found: " + str(len(docker_hosts)))
 
 for el in docker_hosts:
     docker_host = Entity(el["entity"])
@@ -30,9 +30,8 @@ for el in docker_hosts:
     entity_filter = "lower(tags.docker-host) = lower('" + docker_host.name + "')"
     entities = entity_service.list(expression=entity_filter, limit=0, tags="*")
 
-    print(" - FOUND {} objects for docker_host={} : {} : elapsed_days={}".format(
-        len(entities), docker_host.name, docker_host.lastInsertDate, elapsed_days)
-    )
+    print(" - FOUND " + str(len(entities)) + " objects for docker_host= " + docker_host.name +
+          " : " + docker_host.lastInsertDate + " : elapsed_days= " + str(elapsed_days))
 
     if elapsed_days < 7:
         print(" - RETAIN (do not delete): " + docker_host.name)
@@ -41,9 +40,7 @@ for el in docker_hosts:
     print(" - DELETE objects for docker_host= " + docker_host.name)
 
     for entity in entities:
-        print("- Deleting {} : {} : {}".format(
-            entity.tags.get('docker-type', ''), entity.name, entity.tags.get('name', ''))
-        )
+        print("- Deleting " + entity.tags.get('docker-type', '') + " : " + entity.name + " : " + entity.tags.get('name', ''))
         tags_printer.pprint(entity.tags)
         # Uncomment next line to delete
         #entity_service.delete(entity)
