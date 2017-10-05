5c4d2648dc2ac2a571e13a96b02b9368ba4764f3
rmakulov <rustam.makulov@axibase.com>
2017-10-05 13:27:39 +0300

    Update examples

--- examples/entities_without_last_insert.py
+++ examples/entities_without_last_insert.py
@@ -1,19 +1,15 @@
-from __future__ import print_function
-
-import os
-from os import path
-
-
-from atsd_client import connect_url
+from atsd_client import connect, connect_url
 from atsd_client.services import EntitiesService
 
 connection = connect_url('https://atsd_hostname:8443', 'user', 'pwd')
+# connection = connect()
+# connection = atsd_client.connect('/home/axibase/connection.properties')
+
 entities_service = EntitiesService(connection)
 entity_list = entities_service.list()
 
 required_entities = [entity for entity in entity_list if entity.lastInsertDate is None]
 
-with open(os.path.splitext(path.abspath(__file__))[0] + ".txt", 'w') as f:
-    print("Entities count: %d, %d without last insert date." % (len(entity_list), len(required_entities)), file=f)
-    for entity in required_entities:
-        print(entity.name, file=f)
+print("Entities count: %d, %d without last insert date." % (len(entity_list), len(required_entities)))
+for entity in required_entities:
+    print(entity.name)
--- examples/find_lagging_agents.py
+++ examples/find_lagging_agents.py
@@ -1,23 +1,20 @@
-from __future__ import print_function
-
-import os
-from os import path
-
-from atsd_client import connect_url
+from atsd_client import connect, connect_url
 from atsd_client.services import MetricsService, EntitiesService
 
 connection = connect_url('https://atsd_hostname:8443', 'user', 'pwd')
+# connection = connect()
+# connection = atsd_client.connect('/home/axibase/connection.properties')
 
 agents = ['nurswgvml007', 'nurswgvml010']
 
 entities_service = EntitiesService(connection)
 metrics_service = MetricsService(connection)
 
-with open(os.path.splitext(path.abspath(__file__))[0] + ".txt", 'w') as f:
-    for agent in agents:
-        metrics = entities_service.metrics(agent)
-        for metric in metrics:
-            series = metrics_service.series(metric, agent)
-            for s in series:
-                if s.get_elapsed_minutes() > 24*60:
-                    print(s, file=f)
+for agent in agents:
+    metrics = entities_service.metrics(agent)
+    for metric in metrics:
+        series = metrics_service.series(metric, agent)
+        for s in series:
+            if s.get_elapsed_minutes() > 24 * 60:
+                print("metric: '%s' entity: '%s' tags: %s lastInsertDate: %s" % (
+                    s.metric, s.entity, s.tags, s.lastInsertDate))
