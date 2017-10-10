650821158a5d9e9cf6dfd362ef2d5a4a3d63afb3
rmakulov <rustam.makulov@axibase.com>
2017-10-10 16:41:30 +0300

    Remove timezone

--- examples/find_broken_retention.py
+++ examples/find_broken_retention.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
-from dateutil.tz import tzlocal
-# from prettytable import PrettyTable
+from prettytable import PrettyTable
 
 from atsd_client import connect, connect_url
 from atsd_client.models import SeriesQuery, SeriesFilter, EntityFilter, DateFilter, ControlFilter, to_iso_local
@@ -11,7 +10,7 @@ Find series with data older than `now - (metric.retentionDays + grace_interval_d
 '''
 
 connection = connect_url('https://atsd_hostname:8443', 'user', 'pwd')
-# connection = atsd_client.connect('/home/axibase/connection.properties')
+# connection = connect('/home/axibase/connection.properties')
 
 svc = SeriesService(connection)
 metric_service = MetricsService(connection)
@@ -21,12 +20,11 @@ series_count = 0
 # ATSD expired data removal schedule frequency, default is one day
 grace_interval_days = 1
 
-print('Metric, Entity, Tags, Retention Days, Threshold, Presented Sample Date')
-# t = PrettyTable(['Metric', 'Entity', 'Tags', 'Retention Days', 'Threshold', 'Presented Sample Date'])
+t = PrettyTable(['Metric', 'Entity', 'Tags', 'Retention Days', 'Threshold', 'Presented Sample Date'])
 for metric in metric_list:
     if metric.enabled and metric.persistent and metric.retentionDays != 0:
         # calculate datetime before which there should be data
-        threshold = datetime.now(tzlocal()) - timedelta(days=metric.retentionDays + grace_interval_days)
+        threshold = datetime.now() - timedelta(days=metric.retentionDays + grace_interval_days)
 
         # query series with current metric and all entities from the beginning up to threshold
         # enough to get at least one value so limit set to 1
@@ -39,7 +37,6 @@ for metric in metric_list:
         for sl in series_list:
             if len(sl.data) > 0:
                 series_count += 1
-                print("%s, %s, %s, %s, %s, %s " % (sl.metric, sl.entity, sl.tags, metric.retentionDays, threshold, to_iso_local(sl.data[0].t)))
-                # t.add_row([sl.metric, sl.entity, sl.tags, metric.retentionDays, threshold, to_iso_local(sl.data[0].t)])
-# print t
+                t.add_row([sl.metric, sl.entity, sl.tags, metric.retentionDays, threshold, to_iso_local(sl.data[0].t)])
+print t
 print("\nSeries count with broken retention date is %d." % series_count)
