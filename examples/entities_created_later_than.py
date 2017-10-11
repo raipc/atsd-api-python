313f8cefab52383884f4b4ec5efb562c54601b2d
rmakulov <rustam.makulov@axibase.com>
2017-10-11 22:59:30 +0300

    Filter by createdDate on the server

--- examples/entities_created_later_than.py
+++ examples/entities_created_later_than.py
@@ -4,7 +4,7 @@ from atsd_client import connect, connect_url
 from atsd_client.services import EntitiesService
 
 '''
-Locate a collection of entities that have been created after specific date.
+Locate a collection of entities that have been created after specified date.
 Connection.properties will be taken from the same folder where script is.
 '''
 
@@ -12,18 +12,10 @@ connection = connect_url('https://atsd_hostname:8443', 'user', 'pwd')
 # connection = connect()
 # connection = atsd_client.connect('/home/axibase/connection.properties')
 
-# set specific date
-specific_date = parser.parse('2017-10-01T00:00:00Z')
-
 entities_service = EntitiesService(connection)
-# query all entities
-entity_list = entities_service.list()
+# query all entities created after specified date
+entity_list = entities_service.list(expression="createdDate > '2017-10-01T00:00:00Z'")
 
-# select entities created after specific_date
-required_entities = [entity for entity in entity_list if
-                     entity.createdDate is not None and entity.createdDate > specific_date]
-
-for entity in required_entities:
-    print(entity.name)
-
-print("\nEntities created later than %s." % (specific_date.isoformat()))
+print('entity.name, entity.label')
+for entity in entity_list:
+    print('%s, %s' % (entity.name, entity.label if entity.label is not None else ''))
--- examples/metrics_created_later_than.py
+++ examples/metrics_created_later_than.py
@@ -4,7 +4,7 @@ from atsd_client import connect, connect_url
 from atsd_client.services import MetricsService
 
 '''
-Locate a collection of metrics that have been created after specific date.
+Locate a collection of metrics that have been created after specified date.
 Connection.properties will be taken from the same folder where script is.
 '''
 
@@ -12,18 +12,11 @@ connection = connect_url('https://atsd_hostname:8443', 'user', 'pwd')
 # connection = connect()
 # connection = atsd_client.connect('/home/axibase/connection.properties')
 
-# set specific date
-specific_date = parser.parse('2017-10-01T00:00:00Z')
-
 metrics_service = MetricsService(connection)
-# query all metrics
-metric_list = metrics_service.list()
+# query all metrics created after specified_date
+metric_list = metrics_service.list(expression="createdDate > '2017-10-01T00:00:00Z'")
 
-# select entities created after specific_date
-required_metrics = [metric for metric in metric_list if
-                    metric.createdDate is not None and metric.createdDate > specific_date]
-
-for metric in required_metrics:
+print('metric.name')
+for metric in metric_list:
     print(metric.name)
 
-print("Metrics created later than %s." % (specific_date.isoformat()))
