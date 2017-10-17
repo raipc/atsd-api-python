93e65398f92e3d7978ca5e37bbce2e02359d4a6b
rmakulov <rustam.makulov@axibase.com>
2017-10-17 10:45:51 +0300

    Move filter to the server

--- examples/find_broken_retention.py
+++ examples/find_broken_retention.py
@@ -15,28 +15,28 @@ connection = connect_url('https://atsd_hostname:8443', 'user', 'password')
 svc = SeriesService(connection)
 metric_service = MetricsService(connection)
 
-metric_list = metric_service.list()
+metric_expression = "enabled AND persistent AND retentionDays > 0"
+metric_list = metric_service.list(expression=metric_expression)
 series_count = 0
 # ATSD expired data removal schedule frequency, default is one day
 grace_interval_days = 1
 
 t = PrettyTable(['Metric', 'Entity', 'Tags', 'Retention Days', 'Threshold', 'Presented Sample Date'])
 for metric in metric_list:
-    if metric.enabled and metric.persistent and metric.retentionDays != 0:
-        # calculate datetime before which there should be data
-        threshold = datetime.now() - timedelta(days=metric.retentionDays + grace_interval_days)
+    # calculate datetime before which there should be data
+    threshold = datetime.now() - timedelta(days=metric.retentionDays + grace_interval_days)
 
-        # query series with current metric and all entities from the beginning up to threshold
-        # enough to get at least one value so limit set to 1
-        sf = SeriesFilter(metric=metric.name)
-        ef = EntityFilter(entity='*')
-        df = DateFilter(startDate="1970-01-01T00:00:00Z", endDate=threshold)
-        cf = ControlFilter(limit=1)
-        query = SeriesQuery(series_filter=sf, entity_filter=ef, date_filter=df, control_filter=cf)
-        series_list = svc.query(query)
-        for sl in series_list:
-            if len(sl.data) > 0:
-                series_count += 1
-                t.add_row([sl.metric, sl.entity, sl.tags, metric.retentionDays, threshold, to_iso(sl.data[0].t)])
+    # query series with current metric and all entities from the beginning up to threshold
+    # enough to get at least one value so limit set to 1
+    sf = SeriesFilter(metric=metric.name)
+    ef = EntityFilter(entity='*')
+    df = DateFilter(startDate="1970-01-01T00:00:00Z", endDate=threshold)
+    cf = ControlFilter(limit=1)
+    query = SeriesQuery(series_filter=sf, entity_filter=ef, date_filter=df, control_filter=cf)
+    series_list = svc.query(query)
+    for sl in series_list:
+        if len(sl.data) > 0:
+            series_count += 1
+            t.add_row([sl.metric, sl.entity, sl.tags, metric.retentionDays, threshold, to_iso(sl.data[0].t)])
 print(t)
 print("\nSeries count with broken retention date is %d." % series_count)
