63ccb1cd6f85415c19fa74f2ce3580b9d103346b
rmakulov <rustam.makulov@axibase.com>
2017-10-31 15:02:09 +0300

    Detect outliers: add next window

--- examples/find_outliers_manual.py
+++ examples/find_outliers_manual.py
@@ -1,52 +1,73 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from atsd_client import connect_url
 from atsd_client.models import SeriesQuery, SeriesFilter, EntityFilter, DateFilter
 from atsd_client.services import MetricsService, SeriesService
 
-interval_days = 10
-min_window_size = 5
-
 # Connect to an ATSD server
 connection = connect_url('https://atsd_hostname:8443', 'user', 'password')
+
+# set minimum window size to make decision
+min_window_size = 5
+# 10 days interval in milliseconds
+interval_ms = 10 * 24 * 60 * 60 * 1000
 
 svc = SeriesService(connection)
 metric_service = MetricsService(connection)
 
 # set metric and entity
 metric = 'ca.daily.reservoir_storage_af'
-entity = 'ca.nhg'
+entity = 'ca.oro'
 
+print('Metric: %s\nEntity: %s\n' % (metric, entity))
+
+# prepare query to retrieve data
 sf = SeriesFilter(metric=metric, exact_match=True)
 ef = EntityFilter(entity=entity)
 df = DateFilter(start_date="1970-01-01T00:00:00Z", end_date=datetime.now())
 
 query = SeriesQuery(series_filter=sf, entity_filter=ef, date_filter=df)
-series_list = svc.query(query)
+series, = svc.query(query)
 
-data = series_list[0].data
+# filter data to exclude not a numbers and non-positive values
+data = [s for s in series.data if s.v is not None and s.v > 0]
 
 for i, sample in enumerate(data):
-    if sample.v is None:
-        continue
 
     # prepare data for previous 10 days
-    window = []
-    for idx, s in enumerate(data[:i]):
-        if s.v is not None and s.v > 0 \
-                and (sample.get_date() - timedelta(days=interval_days)) <= s.get_date() < sample.get_date():
-            window.append(s)
+    previous_window = []
+    for s in data[:i]:
+        if (sample.t - interval_ms) <= s.t < sample.t:
+            previous_window.append(s)
 
-    if len(window) < min_window_size:
+    # calculate max delta between nearest samples during the previous days
+    max_delta_before = -1
+    for j, s in enumerate(previous_window[:-1]):
+        delta = abs(s.v - previous_window[j + 1].v)
+        if delta > max_delta_before:
+            max_delta_before = delta
+
+    # prepare data for next 10 days
+    next_window = []
+    for s in data[i:]:
+        if (sample.t + interval_ms) >= s.t > sample.t:
+            next_window.append(s)
+
+    # calculate max delta between nearest samples during the next days
+    max_delta_after = -1
+    for j, s in enumerate(next_window[1:], 1):
+        delta = abs(s.v - next_window[j - 1].v)
+        if delta > max_delta_after:
+            max_delta_after = delta
+
+    # check that we have enough data to make decision
+    if len(previous_window) < min_window_size or len(next_window) < min_window_size:
         continue
 
-    # calculate max delta between nearest samples in prepared data
-    max_delta = -1
-    for j, s in enumerate(window[:-1]):
-        delta = abs(s.v - window[j + 1].v)
-        if delta > max_delta:
-            max_delta = delta
+    before_delta = abs(sample.v - previous_window[-1].v)
+    after_delta = abs(sample.v - next_window[0].v)
 
-    if abs(sample.v - window[-1].v) > 5 * max_delta:
+    if before_delta > 5 * max_delta_before and after_delta > 5 * max_delta_after:
         print('%s %s' % (sample.get_date(), sample.v))
-        sample.v = None
+        # replace outlier to improve the data
+        sample.v = previous_window[-1].v
