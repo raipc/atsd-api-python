26226aca1af9571e6e091abc26b31d579972d4a2
rmakulov <rustam.makulov@axibase.com>
2017-11-29 19:40:57 +0300

    Expose parameters, add annotation commands

--- examples/bioreactor_model.py
+++ examples/bioreactor_model.py
@@ -8,23 +8,6 @@ from atsd_client import connect_url
 from atsd_client.models import Entity, Series, Sample
 from atsd_client.services import EntitiesService, SeriesService
 
-ASSET_COUNT = 10
-SITE_COUNT = 2
-BUILDING_PER_SITE_COUNT = 2
-
-MIN_TIME = parse('2017-11-15T23:02:00Z')
-MAX_TIME = parse('2017-11-17T02:02:00Z')
-
-min_jacket_temperature = -10
-max_jacket_temperature = 80
-min_agitator_speed = 0
-max_agitator_speed = 5
-min_product_temperature = 15
-max_product_temperature = 40
-
-sites = ['svl', 'nur']
-buildings = [['A', 'B'], ['C', 'D']]
-
 
 def positive_spline(diff_value, start_value, t, l, x):
     return diff_value * (((x - t).total_seconds() / l.total_seconds()) ** 3) + start_value
@@ -38,24 +21,16 @@ def negative_spline(diff_value, end_value, t, l, x):
     return diff_value * ((1 - ((x - t).total_seconds() / l.total_seconds())) ** 3) + end_value
 
 
-def negative_inv_spline(diff_value, end_value, t, l, x):
-    return diff_value * (1-(x - t).total_seconds() / l.total_seconds() ** 3) + end_value
-
-
-def negative_smooth_spline(diff_value, end_value, t, l, x):
-    return diff_value * (1-(x - t).total_seconds() / l.total_seconds()) + end_value
-
-
 def linear(diff_value, start_value, t, l, x):
     return start_value if diff_value == 0 else diff_value * (x - t).total_seconds() / l.total_seconds() + start_value
 
 
-def rand_int(n=65535):
-    return int(random.random() * n)
+def rand_int(n=65535, start=0):
+    return int(random.random() * (n - start)) + start
 
 
 def next_time(time):
-    return time + timedelta(seconds=rand_int(4) + 60)
+    return time + timedelta(seconds=60 + rand_int(4))
 
 
 def next_time_p(procedure, time):
@@ -76,38 +51,57 @@ class SplineHolder:
         return None
 
 
+ASSET_COUNT = 1
+SITE_COUNT = 2
+BUILDING_PER_SITE_COUNT = 2
+BATCH_COUNT = 3
+
+MIN_TIME = parse('2017-11-15T23:02:00Z')
+MAX_TIME = parse('2017-11-19T02:02:00Z')
+
+MIN_JACKET_TEMPERATURE = -10
+MAX_JACKET_TEMPERATURE = 80
+MIN_AGITATOR_SPEED = 0
+MAX_AGITATOR_SPEED = 5
+MIN_PRODUCT_TEMPERATURE = 15
+MAX_PRODUCT_TEMPERATURE = 40
+
+sites = ['svl', 'nur']
+buildings = [['A', 'B'], ['C', 'D']]
+
 connection = connect_url('https://atsd_hostname:8443', 'user', 'password')
 entities_service = EntitiesService(connection)
 svc = SeriesService(connection)
 
+AGITATION_COUNT = rand_int(5, 2)
+BATCH_IDLE_DURATION = rand_int(8, 1)
+STAGE_1_DURATION = rand_int(8, 4)
+STAGE_2_DURATION = rand_int(10, 5)
+STAGE_3_DURATION = rand_int(24, 16)
+# Fit batches into the specified range or in proportionally
+SUIT_BATCHES = True
+
+print('AGITATION_COUNT = %s' % AGITATION_COUNT)
+print('BATCH_IDLE = %s' % BATCH_IDLE_DURATION)
+print('STAGE_1_DURATION = %s' % STAGE_1_DURATION)
+print('STAGE_2_DURATION = %s' % STAGE_2_DURATION)
+print('STAGE_3_DURATION = %s' % STAGE_3_DURATION)
+print('SUIT_BATCHES = %s' % SUIT_BATCHES)
+
+stage_2_leaps = []
+for i in range(AGITATION_COUNT):
+    stage_2_leaps.append(['Stage 2: Enable agitator %d' % i, 3 / 4])
+    stage_2_leaps.append((['Stage 2: Product temperature extremum %d' % i, 1 / 4]))
+    stage_2_leaps.append((['Stage 2: Disable agitator %d' % i, 1]))
+
 procedures = [['Stage 1: Jacket Heating', 1],
               ['Stage 1: Static Drying', 9],
-
-              ['Stage 2: Enable agitator 1', 3 / 4],
-              ['Stage 2: Product temperature extremum 1', 1 / 4],
-              ['Stage 2: Disable agitator 1', 1],
-
-              ['Stage 2: Enable agitator 2', 3 / 4],
-              ['Stage 2: Product temperature extremum 2', 1 / 4],
-              ['Stage 2: Disable agitator 2', 1],
-
-              ['Stage 2: Enable agitator 3', 3 / 4],
-              ['Stage 2: Product temperature extremum 3', 1 / 4],
-              ['Stage 2: Disable agitator 3', 1],
-
-              ['Stage 2: Enable agitator 4', 3 / 4],
-              ['Stage 2: Product temperature extremum 4', 1 / 4],
-              ['Stage 2: Disable agitator 4', 1],
-
-              ['Stage 2: Enable agitator 5', 3 / 4],
-              ['Stage 2: Product temperature extremum 5', 1 / 4],
-              ['Stage 2: Disable agitator 5', 1],
-
+              *stage_2_leaps,
               ['Stage 2: End', 1],
               ['Stage 3: Product Cooled Down', 2],
               ['Stage 3: Product Heating', 2],
-              ['Stage 3: Disable agitator 6', 1],
-              ['Stage 3: Enable agitator 6', 15],
+              ['Stage 3: Disable agitator 0', 1],
+              ['Stage 3: Enable agitator 0', 15],
               ['Stage 3: Agitator maximum', 1 / 2],
               ['Stage 3: Agitator normal', 1 / 4],
               ['Stage 3: Agitator disable', 1 / 8],
@@ -115,135 +109,128 @@ procedures = [['Stage 1: Jacket Heating', 1],
               ['Stage 3: Jacket Cooled Down', 1],
               ['Stage 3: Jacket Heating', 1],
               ['Stage 3: Product Cooling', 2],
-              ['Stage 3: Disable agitator 7', 1],
-              ['Stage 3: Disable agitator 8', 5]]
+              ['Stage 3: Disable agitator 1', 1],
+              ['Stage 3: Disable agitator 2', 5],
+              ['Inactive', 1]]
 
-total_units = 0
+unit_minutes = ((MAX_TIME - MIN_TIME) / (
+    STAGE_1_DURATION + STAGE_2_DURATION + STAGE_3_DURATION + BATCH_IDLE_DURATION)).total_seconds() / BATCH_COUNT / 60
+
+first_stage_length = 0
+second_stage_length = 0
+third_stage_length = 0
 for [name, duration] in procedures:
-    total_units = total_units + duration
+    if name.startswith('Stage 1'):
+        first_stage_length = first_stage_length + duration
+    elif name.startswith('Stage 2'):
+        second_stage_length = second_stage_length + duration
+    elif name.startswith('Stage 3'):
+        third_stage_length = third_stage_length + duration
 
-unit_minutes = ((MAX_TIME - MIN_TIME) / total_units).total_seconds() / 60
-print(unit_minutes)
+for idx, [name, duration] in enumerate(procedures):
+    if name == 'Inactive':
+        duration = duration * BATCH_IDLE_DURATION
+    elif name.startswith('Stage 1'):
+        duration = duration * STAGE_1_DURATION / first_stage_length
+    elif name.startswith('Stage 2'):
+        duration = duration * STAGE_2_DURATION / second_stage_length
+    elif name.startswith('Stage 3'):
+        duration = duration * STAGE_3_DURATION / third_stage_length
+    procedures[idx] = [name, duration * unit_minutes if SUIT_BATCHES else 60]
 
-procedures = [[name, duration * unit_minutes] for [name, duration] in procedures]
+jacket_temperature_leaps = dict([(x[0], partial(linear, 0, MAX_JACKET_TEMPERATURE)) for x in stage_2_leaps])
+agitator_speed_leaps = dict(
+    [(x[0], partial(linear, 0, 0)) if idx % 3 == 2 else (x[0], partial(linear, 0, 2)) for idx, x in
+     enumerate(stage_2_leaps)])
+product_temperature_leaps = {}
+
+middle = 21.8
+step = (28.1 - middle) / AGITATION_COUNT
+negative_step = 4.4 + step
+for idx in range(0, len(stage_2_leaps), 3):
+    enable = stage_2_leaps[idx]
+    extremum = stage_2_leaps[idx + 1]
+    disable = stage_2_leaps[idx + 2]
+    if idx == 0:
+        product_temperature_leaps[enable[0]] = partial(positive_inv_spline, 2, 26.6)
+        product_temperature_leaps[extremum[0]] = partial(positive_spline, -11, 28.6)
+        product_temperature_leaps[disable[0]] = partial(positive_spline, 4.2, 17.6)
+    elif idx == len(stage_2_leaps):
+        product_temperature_leaps[enable[0]] = partial(linear, 3.9, middle)
+        product_temperature_leaps[extremum[0]] = partial(positive_spline, -4.7, 24.7)
+        product_temperature_leaps[disable[0]] = partial(positive_spline, 6.1, 20.0)
+    elif idx > len(stage_2_leaps) / 2:
+        multiplier = (idx - len(stage_2_leaps) / 2) / 3
+        product_temperature_leaps[enable[0]] = partial(linear, step, middle + step * multiplier)
+        product_temperature_leaps[extremum[0]] = partial(positive_spline, -negative_step,
+                                                         middle + step * (multiplier + 1))
+        product_temperature_leaps[disable[0]] = partial(positive_spline, negative_step - step * multiplier,
+                                                        middle + step * (multiplier + 1) - negative_step)
+    else:
+        product_temperature_leaps[enable[0]] = partial(linear, step, middle)
+        product_temperature_leaps[extremum[0]] = partial(positive_spline, -negative_step, 17.4 + negative_step)
+        product_temperature_leaps[disable[0]] = partial(positive_spline, 4.4, 17.4)
 
 metrics = [
     ['jacket_temperature', {
-        'Stage 1: Jacket Heating': partial(positive_spline, 80, 0),
-        'Stage 1: Static Drying': partial(linear, 0, 80),
-
-        'Stage 2: Enable agitator 1': partial(linear, 0, 80),
-        'Stage 2: Product temperature extremum 1': partial(linear, 0, 80),
-        'Stage 2: Disable agitator 1': partial(linear, 0, 80),
-
-        'Stage 2: Enable agitator 2': partial(linear, 0, 80),
-        'Stage 2: Product temperature extremum 2': partial(linear, 0, 80),
-        'Stage 2: Disable agitator 2': partial(linear, 0, 80),
-
-        'Stage 2: Enable agitator 3': partial(linear, 0, 80),
-        'Stage 2: Product temperature extremum 3': partial(linear, 0, 80),
-        'Stage 2: Disable agitator 3': partial(linear, 0, 80),
-
-        'Stage 2: Enable agitator 4': partial(linear, 0, 80),
-        'Stage 2: Product temperature extremum 4': partial(linear, 0, 80),
-        'Stage 2: Disable agitator 4': partial(linear, 0, 80),
-
-        'Stage 2: Enable agitator 5': partial(linear, 0, 80),
-        'Stage 2: Product temperature extremum 5': partial(linear, 0, 80),
-        'Stage 2: Disable agitator 5': partial(linear, 0, 80),
-
-        'Stage 2: End': partial(linear, 0, 80),
-        'Stage 3: Product Cooled Down': partial(linear, 0, 80),
-        'Stage 3: Product Heating': partial(linear, 0, 80),
-        'Stage 3: Disable agitator 6': partial(linear, 0, 80),
-        'Stage 3: Enable agitator 6': partial(linear, 0, 80),
-        'Stage 3: Agitator maximum': partial(linear, 0, 80),
-        'Stage 3: Agitator normal': partial(linear, 0, 80),
-        'Stage 3: Agitator disable': partial(linear, 0, 80),
-        'Stage 3: Agitator enable': partial(linear, 0, 80),
-        'Stage 3: Jacket Cooled Down': partial(negative_spline, 90, -10),
-        'Stage 3: Jacket Heating': partial(positive_inv_spline, 5, -10),
+        'Stage 1: Jacket Heating': partial(positive_spline, MAX_JACKET_TEMPERATURE, 0),
+        'Stage 1: Static Drying': partial(linear, 0, MAX_JACKET_TEMPERATURE),
+        **jacket_temperature_leaps,
+        'Stage 2: End': partial(linear, 0, MAX_JACKET_TEMPERATURE),
+        'Stage 3: Product Cooled Down': partial(linear, 0, MAX_JACKET_TEMPERATURE),
+        'Stage 3: Product Heating': partial(linear, 0, MAX_JACKET_TEMPERATURE),
+        'Stage 3: Disable agitator 0': partial(linear, 0, MAX_JACKET_TEMPERATURE),
+        'Stage 3: Enable agitator 0': partial(linear, 0, MAX_JACKET_TEMPERATURE),
+        'Stage 3: Agitator maximum': partial(linear, 0, MAX_JACKET_TEMPERATURE),
+        'Stage 3: Agitator normal': partial(linear, 0, MAX_JACKET_TEMPERATURE),
+        'Stage 3: Agitator disable': partial(linear, 0, MAX_JACKET_TEMPERATURE),
+        'Stage 3: Agitator enable': partial(linear, 0, MAX_JACKET_TEMPERATURE),
+        'Stage 3: Jacket Cooled Down': partial(negative_spline, 90, MIN_JACKET_TEMPERATURE),
+        'Stage 3: Jacket Heating': partial(positive_inv_spline, 5, MIN_JACKET_TEMPERATURE),
         'Stage 3: Product Cooling': partial(linear, 0, -5),
-        'Stage 3: Disable agitator 7': partial(positive_inv_spline, 5, -5),
-        'Stage 3: Disable agitator 8': partial(linear, 0, 0)
+        'Stage 3: Disable agitator 1': partial(positive_inv_spline, 5, -5),
+        'Stage 3: Disable agitator 2': partial(linear, 0, 0),
+        'Inactive': partial(linear, 0, 0)
     }],
     ['agitator_speed', {
-        'Stage 1: Jacket Heating': partial(linear, 0, 0),
-        'Stage 1: Static Drying': partial(linear, 0, 0),
-
-        'Stage 2: Enable agitator 1': partial(linear, 0, 2),
-        'Stage 2: Product temperature extremum 1': partial(linear, 0, 2),
-        'Stage 2: Disable agitator 1': partial(linear, 0, 0),
-
-        'Stage 2: Enable agitator 2': partial(linear, 0, 2),
-        'Stage 2: Product temperature extremum 2': partial(linear, 0, 2),
-        'Stage 2: Disable agitator 2': partial(linear, 0, 0),
-
-        'Stage 2: Enable agitator 3': partial(linear, 0, 2),
-        'Stage 2: Product temperature extremum 3': partial(linear, 0, 2),
-        'Stage 2: Disable agitator 3': partial(linear, 0, 0),
-
-        'Stage 2: Enable agitator 4': partial(linear, 0, 2),
-        'Stage 2: Product temperature extremum 4': partial(linear, 0, 2),
-        'Stage 2: Disable agitator 4': partial(linear, 0, 0),
-
-        'Stage 2: Enable agitator 5': partial(linear, 0, 2),
-        'Stage 2: Product temperature extremum 5': partial(linear, 0, 2),
-        'Stage 2: Disable agitator 5': partial(linear, 0, 0),
-
+        'Stage 1: Jacket Heating': partial(linear, 0, MIN_AGITATOR_SPEED),
+        'Stage 1: Static Drying': partial(linear, 0, MIN_AGITATOR_SPEED),
+        **agitator_speed_leaps,
         'Stage 2: End': partial(linear, 0, 2),
         'Stage 3: Product Cooled Down': partial(linear, 0, 2),
         'Stage 3: Product Heating': partial(linear, 0, 2),
-        'Stage 3: Disable agitator 6': partial(linear, 0, 0),
-        'Stage 3: Enable agitator 6': partial(linear, 0, 2),
-        'Stage 3: Agitator maximum': partial(linear, 0, 5),
+        'Stage 3: Disable agitator 0': partial(linear, 0, MIN_AGITATOR_SPEED),
+        'Stage 3: Enable agitator 0': partial(linear, 0, 2),
+        'Stage 3: Agitator maximum': partial(linear, 0, MAX_AGITATOR_SPEED),
         'Stage 3: Agitator normal': partial(linear, 0, 2),
-        'Stage 3: Agitator disable': partial(linear, 0, 0),
+        'Stage 3: Agitator disable': partial(linear, 0, MIN_AGITATOR_SPEED),
         'Stage 3: Agitator enable': partial(linear, 0, 2),
         'Stage 3: Jacket Cooled Down': partial(linear, 0, 2),
         'Stage 3: Jacket Heating': partial(linear, 0, 2),
         'Stage 3: Product Cooling': partial(linear, 0, 2),
-        'Stage 3: Disable agitator 7': partial(linear, 0, 0),
-        'Stage 3: Disable agitator 8': partial(linear, 0, 0)
+        'Stage 3: Disable agitator 1': partial(linear, 0, MIN_AGITATOR_SPEED),
+        'Stage 3: Disable agitator 2': partial(linear, 0, MIN_AGITATOR_SPEED),
+        'Inactive': partial(linear, 0, MIN_AGITATOR_SPEED)
     }],
     ['product_temperature', {
-        'Stage 1: Jacket Heating': partial(positive_spline, 2.4, 15),
+        'Stage 1: Jacket Heating': partial(positive_spline, 2.4, MIN_PRODUCT_TEMPERATURE),
         'Stage 1: Static Drying': partial(positive_inv_spline, 9.2, 17.4),
-
-        'Stage 2: Enable agitator 1': partial(positive_inv_spline, 2, 26.6),
-        'Stage 2: Product temperature extremum 1': partial(negative_inv_spline, 11, 17.6),
-        'Stage 2: Disable agitator 1': partial(positive_spline, 4.2, 17.6),
-
-        'Stage 2: Enable agitator 2': partial(linear, 1, 21.8),
-        'Stage 2: Product temperature extremum 2': partial(negative_inv_spline, 5.4, 17.4),
-        'Stage 2: Disable agitator 2': partial(positive_spline, 4.2, 17.4),
-
-        'Stage 2: Enable agitator 3': partial(linear, 1, 21.6),
-        'Stage 2: Product temperature extremum 3': partial(negative_inv_spline, 5, 17.6),
-        'Stage 2: Disable agitator 3': partial(positive_spline, 4.2, 17.6),
-
-        'Stage 2: Enable agitator 4': partial(linear, 1, 21.8),
-        'Stage 2: Product temperature extremum 4': partial(negative_inv_spline, 5, 17.8),
-        'Stage 2: Disable agitator 4': partial(positive_spline, 5.6, 18.1),
-
-        'Stage 2: Enable agitator 5': partial(linear, 1, 23.7),
-        'Stage 2: Product temperature extremum 5': partial(negative_inv_spline, 4.7, 20.0),
-        'Stage 2: Disable agitator 5': partial(positive_spline, 6.1, 20.0),
-
+        **product_temperature_leaps,
         'Stage 2: End': partial(positive_inv_spline, 2, 26.1),
-        'Stage 3: Product Cooled Down': partial(negative_spline, 13.1, 15.0),
-        'Stage 3: Product Heating': partial(positive_spline, 3.8, 15.0),
-        'Stage 3: Disable agitator 6': partial(positive_inv_spline, 7.3, 18.8),
-        'Stage 3: Enable agitator 6': partial(positive_inv_spline, 12.5, 26.1),
-        'Stage 3: Agitator maximum': partial(positive_inv_spline, 1.3, 38.6),
-        'Stage 3: Agitator normal': partial(positive_inv_spline, 1, 39.3),
-        'Stage 3: Agitator disable': partial(negative_inv_spline, 1, 39.3),
+        'Stage 3: Product Cooled Down': partial(negative_spline, 13.1, MIN_PRODUCT_TEMPERATURE),
+        'Stage 3: Product Heating': partial(positive_spline, 3.8, MIN_PRODUCT_TEMPERATURE),
+        'Stage 3: Disable agitator 0': partial(positive_inv_spline, 7.3, 18.8),
+        'Stage 3: Enable agitator 0': partial(positive_inv_spline, 12.5, 26.1),
+        'Stage 3: Agitator maximum': partial(positive_inv_spline, 1, 38.6),
+        'Stage 3: Agitator normal': partial(positive_inv_spline, 0.4, 39.6),
+        'Stage 3: Agitator disable': partial(positive_spline, -1, MAX_PRODUCT_TEMPERATURE),
         'Stage 3: Agitator enable': partial(positive_inv_spline, 0.7, 38.6),
-        'Stage 3: Jacket Cooled Down': partial(linear, 0, 40.0),
-        'Stage 3: Jacket Heating': partial(negative_smooth_spline, 3.3, 36.9),
-        'Stage 3: Product Cooling': partial(negative_smooth_spline, 6.6, 30.1),
-        'Stage 3: Disable agitator 7': partial(negative_smooth_spline, 4, 26.1),
-        'Stage 3: Disable agitator 8': partial(negative_spline, 7.8, 18.3)
+        'Stage 3: Jacket Cooled Down': partial(linear, 0, MAX_PRODUCT_TEMPERATURE),
+        'Stage 3: Jacket Heating': partial(linear, -3.3, MAX_PRODUCT_TEMPERATURE),
+        'Stage 3: Product Cooling': partial(linear, -6.6, 36.7),
+        'Stage 3: Disable agitator 1': partial(linear, -4, 30.1),
+        'Stage 3: Disable agitator 2': partial(negative_spline, 3.1, 23),
+        'Inactive': partial(linear, -8, 23)
     }]
 ]
 
@@ -251,12 +238,13 @@ assets = []
 for i in range(ASSET_COUNT):
     site = rand_int(SITE_COUNT)
     building = rand_int(BUILDING_PER_SITE_COUNT)
-    assets.append({'id': 'axi.asset-%s' % i, 'site': sites[site], 'building': buildings[site][building]})
+    assets.append({'id': 'exp.asset-%s' % i, 'site': sites[site], 'building': buildings[site][building]})
 
 for asset in assets:
     entity = Entity(asset['id'], tags={'site': asset['site'], 'building': asset['building']})
     entities_service.create_or_replace(entity)
 
+batch_id = 1400
 dataCommandSplines = []
 series = []
 
@@ -268,6 +256,23 @@ for asset in assets:
     dataCommandSplines.append([asset, dataSplines])
 
     while t < MAX_TIME:
+        procedure_name = ''
+
+        if proc == 0:
+            series.append(Series(asset['id'], 'exp.Unit_BatchID', data=[Sample(time=t, x=batch_id, value=None)]))
+            batch_id += 1
+            procedure_name = 'Stage 1 Static Drying'
+        elif procedures[proc][0] == 'Inactive':
+            series.append(Series(asset['id'], 'exp.Unit_BatchID', data=[Sample(time=t, x='Inactive', value=None)]))
+        elif procedures[proc][0] == 'Stage 2: Enable agitator 0':
+            procedure_name = 'Stage 2 Intermittent Agitation'
+        elif procedures[proc][0] == 'Stage 3: Product Cooled Down':
+            procedure_name = 'Stage 3 Continuous Agitation'
+
+        if procedure_name:
+            series.append(
+                Series(asset['id'], 'exp.Unit_Procedure', data=[Sample(time=t, x=procedure_name, value=None)]))
+
         next_t = next_time_p(procedures[proc], t)
         for [metric, splines] in metrics:
             dataSplines.put_spline(t, next_t, metric, splines[procedures[proc][0]])
