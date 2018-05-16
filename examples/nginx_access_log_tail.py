acaa1092850eb9211ee1443ff389ece3cf8a0306
rodionos <sergei.rodionov@axibase.com>
2018-05-16 20:00:40 +0300

    Update example with UDF to check elements in the list

--- examples/nginx_access_log_tail.py
+++ examples/nginx_access_log_tail.py
@@ -10,30 +10,38 @@ import logging
 logger = logging.getLogger()
 logger.disabled = True
 
-# nginx must be configured to write logs in CSV format
+# nginx must be configured to write logs in CSV format:
 #
-# geoip_country /etc/nginx/geoip/GeoIP.dat; # the country IP database
-# geoip_city /etc/nginx/geoip/GeoLiteCity.dat; # the city IP database
-# log_format custom '"$remote_addr","$remote_user","$time_iso8601","$host","$request_method","$server_protocol","$scheme","$request_uri",$status,$body_bytes_sent,"$http_refe$
+# geoip_country /etc/nginx/geoip/GeoIP.dat;    # country IP database
+# geoip_city /etc/nginx/geoip/GeoLiteCity.dat; # city IP database
+# geoip_org /etc/nginx/geoip/GeoIPASNum.dat;   # org/network database
+#
+# log_format custom '"$remote_addr","$remote_user","$time_iso8601","$host","$request_method","$server_protocol","$scheme","$request_uri",$status,$body_bytes_sent,"$http_referer","$http_user_agent","$http_x_forwarded_for","$geoip_country_code","$geoip_region",$geoip_region_name","$geoip_city",$geoip_latitude,$geoip_longitude,"$geoip_org"';
 # access_log /var/log/nginx/access.log custom;
 
 # fieldnames must match the custom log_format
-fieldnames = ['remote_addr','remote_user','time_iso8601','host','request_method','server_protocol','scheme','request_uri','status','body_bytes_sent','http_referer','http_user_agent','http_x_forwarded_for','geoip_country_code','geoip_region','geoip_region_name','geoip_city','geoip_latitude','geoip_longitude']
+fieldnames = ['remote_addr','remote_user','time_iso8601','host','request_method','server_protocol','scheme','request_uri','status','body_bytes_sent','http_referer','http_user_agent','http_x_forwarded_for','geoip_country_code','geoip_region','geoip_region_name','geoip_city','geoip_latitude','geoip_longitude', 'geoip_org']
 
 # string lists for ignoring requests from bots, requests to static resources. basic substring match
 ignore_user_agents = ['axibase', 'feedly', 'scollector', 'rome client', 'bot', 'crawl', 'qwant', 'slurp', 'spider', 'webmaster', 'faq', 'apache', 'scan']
-ignore_uris = ['.png', '.js', '.css', '.woff', '.jpg', '.gif', '.ico', 'robots.txt', '/feed/', '.ttf', '.eot', 'wp-content']
-ignore_ip = ['10.20.30.40', '10.20.30.50']
-
-def lookup(addr):
-    try:
-        return socket.gethostbyaddr(addr)
-    except socket.herror:
-        return None, None, None
+ignore_uris = ['.png', '.js', '.css', '.woff', '.jpg', '.gif', '.jpeg', '.ico', '.svg', 'robots.txt', '/feed/', '.ttf', '.eot', 'wp-content', '.lzma', '.xz', '.bz2']
+ignore_ip = ['10.20.30.40', '127.0.0.1']
 
 conn = connect_url('https://atsd_hostname:8443', 'user', 'passwd')
 
 svc = MessageService(conn)
+
+def lookup(addr):
+    try:
+        return socket.gethostbyaddr(addr)[0]
+    except socket.herror:
+        return None
+
+def has_containing_element(lst, str):
+    for item in lst:
+        if item in str:
+            return True
+    return False
 
 # runs forever
 for line in tail("-F", "/var/log/nginx/access.log", _iter=True):
@@ -45,48 +53,52 @@ for line in tail("-F", "/var/log/nginx/access.log", _iter=True):
         if row['request_method'] == 'HEAD':
           continue
 
-        # Ignore HTTP to HTTPS redirects
+        # Ignore HTTP>HTTPS requests
         if row['status'] == '301' and row['scheme'] == 'http':
           continue   
 
-        store=True
-
-        # Ignore records with http_user_agent matching ignore_user_agents list       
+        # Ignore bots
         user_agent = row['http_user_agent'].lower()
-        for ua in ignore_user_agents:
-          if ua in user_agent:
-              store=False
+        if has_containing_element(ignore_user_agents, user_agent):
+          continue
 
-        if store==False:continue
-
-        # Ignore records with request_uri matching ignore_uris list            
+        # Ignore static file requests
         ruri = row['request_uri'].lower()
-        for ur in ignore_uris:
-          if ur in ruri:
-              store=False
+        if has_containing_element(ignore_uris, ruri):
+          continue
 
-        if store==False:continue   
-
-        # Ignore records with remote_addr matching ignore_ip list
+	      # Ignore 404 requests for apt language translation files
+        if row['status'] == '404' and '/public/repository/deb/./' in ruri:
+          continue
+   
+        # Ignore requests from ignored IP addresses
         radr = row['remote_addr']
-        for ar in ignore_ip:
-          if ar in radr:
-              store=False
+        if has_containing_element(ignore_ip, radr):
+          continue 
 
-        if store==False:continue
-
-        # Reverse-resolve IP and set remote_host field in the row object
-        dns = lookup(radr)[0]
-        if dns != None and dns != 'localhost':
+        dns = lookup(radr)
+        if dns != None and 'localhost' not in dns:
           row['remote_host'] = dns
+
+        # Handle proxy-forwarded requests
+        xadr = row['http_x_forwarded_for']       
+        if xadr != None and xadr != '-':
+          if has_containing_element(ignore_ip, xadr):
+            continue
+          xdns = lookup(xadr)
+          if xdns != None and 'localhost' not in xdns:
+            row['remote_x_host'] = xdns
+
+        sev = Severity.NORMAL
 
         # Remove original timestamp in the access log
         del row['time_iso8601']
 
         # Remove tags with default (unknown) value -
-        tags = { k:v for k,v in row.items() if v!='-' }       
+        tags = { k:v for k,v in row.items() if v!='-' }   
 
-        # Replace example.org with the dns name of your server
-        msg = Message('web', 'access.log', 'example.org', datetime.now(), Severity.NORMAL, tags, '')
+        # Replace example.org with actual DNS name
+        msg = Message('web', 'access.log', 'example.org', datetime.now(), sev, tags, '')
+
         res = svc.insert(msg)
-        #print(res, msg.date, msg.tags)
+        print(res, msg.date, msg.tags)
