848e2aefce1bee03453ea127e035cdf4dcb79bbf
Sergei Rodionov <2098022+rodionos@users.noreply.github.com>
2018-07-16 12:58:03 +0300

    Fix spelling

--- examples/version_check.py
+++ examples/version_check.py
@@ -3,17 +3,17 @@ import platform
 import time
 
 '''
-Print python version, timezone and dependencies version.
+Print timezone, python version, dependencies version.
 '''
 
 print('Python version is %s' % platform.python_version())
 print('Current timezone %s\n' % str(time.tzname))
 
-required_packages = ['dateutil', 'requests', 'pandas']
+required_packages = ['dateutil', 'requests', 'pandas', 'atsd_client']
 
 for package in required_packages:
     try:
         p = __import__(package)
-        print('%s vesrion is %s' % (package, p.__version__))
+        print('%s version is %s' % (package, p.__version__))
     except ImportError:
         print('%s uninstalled' % package)
