66da9f5bb2b6ab6aa82c78d0e122610ad9dd6f78
Anna <snowswim@mail.ru>
2018-07-30 17:37:02 +0300

    escaping

--- examples/message_referrer_report.py
+++ examples/message_referrer_report.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import re
 from atsd_client import connect, connect_url
 from atsd_client.models import *
 from atsd_client.services import *
@@ -32,10 +33,10 @@ columns = ['date', 'request_uri', 'http_referer', 'remote_addr', 'geoip_org']
 message_frame = service.query_dataframe(query, columns=columns)
 
 # Exclude rows with specified tag values
-ignore_hosts = '|'.join(
-    ['google', 'axibase.com', 'yandex.ru', 'baidu.com', 'statcounter.com', 'bing.com', 'duckduckgo.com',
-     'jshell.net', 'facebook.com', 't.co', 'dogpile', '78.4'])
-ignore_uris = '|'.join(['/ec2-monitoring', '/?X'])
+ignore_hosts = '|'.join(re.escape(s) for s in
+    ('google', 'axibase.com', 'yandex.ru', 'baidu.com', 'statcounter.com', 'bing.com', 'duckduckgo.com',
+     'jshell.net', 'facebook.com', 't.co', 'dogpile', '78.4'))
+ignore_uris = '|'.join(re.escape(s) for s in ('/ec2-monitoring', '/?X'))
 filtered = message_frame[(~message_frame['http_referer'].str.contains(ignore_hosts)) &
                          (~message_frame['request_uri'].str.contains(ignore_uris))]
 # Rename column names
