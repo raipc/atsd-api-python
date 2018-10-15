569ff5519274f5d7e9ab4c3204e4c11bc5754c20
unrealwork <unrealwork@gmail.com>
2018-10-15 13:03:19 +0300

    fix commented code

--- examples/transforming_schema.py
+++ examples/transforming_schema.py
@@ -68,8 +68,7 @@ for index, row in df.where(pandas.notnull(df), None).iterrows():
     else:
         transformed_commands_batch.append(command)
 
-if len(transformed_commands_batch) > 0:
-    for command in transformed_commands_batch:
+for command in transformed_commands_batch:
         print(command)
 
-# target_command_service.send_commands(transformed_commands)
+# target_command_service.send_commands(transformed_commands_batch)
