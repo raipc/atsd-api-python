2d4cded5efb681ddc42314ad5886432011552c43
Alexander Kostyurenko <alekkostyur@gmail.com>
2019-12-11 19:06:22 +0300

    Removed dates from arguments for series_merge.py

--- examples/series_merge.py
+++ examples/series_merge.py
@@ -1,8 +1,7 @@
 import argparse
 from atsd_client import connect
 from atsd_client.services import SeriesService, EntitiesService, MetricsService
-from atsd_client.models import SeriesQuery, SeriesFilter, EntityFilter, DateFilter, Series
-from atsd_client._time_utilities import to_milliseconds
+from atsd_client.models import SeriesQuery, SeriesFilter, EntityFilter, DateFilter
 
 
 def no_data(series_list):
@@ -22,8 +21,6 @@ parser.add_argument('--src_entity', nargs=1, required=True, help='Source entity'
 parser.add_argument('--dst_entity', nargs=1, required=True, help='Destination entity')
 parser.add_argument('--metric_name', nargs=1, required=True, help='Metric to be cloned')
 parser.add_argument('--tag_expression', nargs=1, required=False, help='Tag expression to be acquired for source series')
-parser.add_argument('--start_date', nargs=1, required=True, help='Start date to retrieve source series')
-parser.add_argument('--end_date', nargs=1, required=True, help='End date to retrieve destination series')
 args = parser.parse_args()
 source_entity = args.src_entity[0]
 dst_entity = args.dst_entity[0]
@@ -31,11 +28,10 @@ metric = args.metric_name[0]
 tag_expression = None
 if args.tag_expression is not None:
     tag_expression = args.tag_expression[0]
-start_date = args.start_date[0]
-end_date = args.end_date[0]
+start_date = '1970-01-01T00:00:00Z'
 
 
-connection = connect('./connection.properties')
+connection = connect('/path/to/connection.properties')
 
 entity_service = EntitiesService(connection)
 
@@ -50,46 +46,41 @@ metric_service = MetricsService(connection)
 if metric_service.get(metric) is None:
     raise NameError("'" + metric + "' metric does not exist")
 
-
 series_service = SeriesService(connection)
-series_filter = SeriesFilter(metric, tag_expression=tag_expression)
-source_entity_filter = EntityFilter(source_entity)
-source_date_filter = DateFilter(start_date, end_date)
-source_query = SeriesQuery(series_filter, source_entity_filter, source_date_filter)
-source_series = series_service.query(source_query)
 
-if no_data(source_series):
-    print('No source series found for these parameters')
+dst_entity_filter = EntityFilter(dst_entity)
+dst_date_filter = DateFilter(start_date, 'now')
+series_filter = SeriesFilter(metric, tag_expression=tag_expression)
+dst_series_query = SeriesQuery(series_filter, dst_entity_filter, dst_date_filter)
+dst_series = series_service.query(dst_series_query)
+
+if no_data(dst_series):
+    print('No destination series found for these parameters')
     exit(1)
 
 
-def err(tags):
-    return ("No series found for '" + dst_entity + "' entity, '" + metric + "' metric and '" +
-          str(tags) + "' tags")
+def err(tags, time):
+    return ("No series found for '" + source_entity + "' entity, '" + metric + "' metric and '" +
+          str(tags) + "' tags before " + time)
 
 
-print(len(source_series))
-dst_entity_filter = EntityFilter(dst_entity)
-for series in source_series:
-    dst_series_filter = SeriesFilter(metric, series.tags)
-    dst_series_query = SeriesQuery(dst_series_filter, dst_entity_filter, source_date_filter)
-    dst_series = series_service.query(dst_series_query)
-    if no_data(dst_series):
-        print(err(series.tags))
+source_entity_filter = EntityFilter(source_entity)
+for series in dst_series:
+    first_dst_time = series.times()[0]  # get first time from sorted collection
+    source_date_filter = DateFilter(start_date, first_dst_time)
+    source_series_filter = SeriesFilter(metric, series.tags)
+    source_query = SeriesQuery(source_series_filter, source_entity_filter, source_date_filter)
+    source_series = series_service.query(source_query)
+
+    if no_data(source_series):
+        print(err(series.tags, first_dst_time))
         continue
-    target_series = get_series_with_tags(dst_series, series.tags)
+
+    target_series = get_series_with_tags(source_series, series.tags)
+
     if target_series is None:
-        print(err(series.tags))
+        print(err(series.tags, first_dst_time))
         continue
-    series.sort()
-    first_time = to_milliseconds(target_series.times()[0])  # get first time from sorted collection
-    samples = []
-    for sample in series.data:
-        if sample.t > first_time:
-            break
-        samples.append(sample)
 
-    if len(samples) == 0:
-        print(err(series.tags) + " before " + str(first_time))
-    series_to_insert = Series(dst_entity, metric, samples, series.tags)
-    series_service.insert(series_to_insert)
+    target_series.entity = dst_entity
+    series_service.insert(target_series)
